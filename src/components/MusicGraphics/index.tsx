a0f172fabf6e79803e0198a051eb8bed68e52305
hayate4th <diabolo.arte3000@gmail.com>
2020-06-05 00:31:16 +0900

    Add welcome button and click animations

--- src/components/MusicGraphics/index.tsx
+++ src/components/MusicGraphics/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import anime from "animejs";
 
@@ -9,6 +9,9 @@ type Props = {
 };
 
 const MusicGraphics: React.FC<Props> = ({ columnProps }) => {
+  const [isMouseOnButton, setIsMouseOnButton] = useState(false);
+  const [isAnimationStopped, setIsAnimationStopped] = useState(false);
+
   useEffect(() => {
     anime({
       targets: ".animated-music-graphics",
@@ -18,10 +21,59 @@ const MusicGraphics: React.FC<Props> = ({ columnProps }) => {
       direction: "alternate",
       loop: true,
     });
+
+    anime({
+      targets: ".animated-title",
+      rotate: [-20, 20],
+      duration: 750,
+      direction: "alternate",
+      easing: "linear",
+      loop: true,
+    });
   }, []);
+
+  useEffect(() => {
+    if (isAnimationStopped) {
+      anime.remove(".animated-music-block");
+      anime.remove(".animated-music-graphics");
+
+      anime({
+        targets: ".animated-music-block",
+        translateX: anime.stagger(3000, {
+          grid: [20, 1],
+          from: "center",
+          axis: "x",
+        }),
+        translateY: anime.stagger(3000, {
+          grid: [20, 9],
+          from: "center",
+          axis: "y",
+        }),
+        scale: 5,
+        duration: 3000,
+        easing: "linear",
+      });
+    }
+  }, [isAnimationStopped]);
+
+  const onMouseEnterHandler = () => setIsMouseOnButton(true);
+
+  const onMouseLeaveHandler = () => setIsMouseOnButton(false);
+
+  const onClickHandler = () => setIsAnimationStopped(true);
 
   return (
     <FlexWrapper className="animated-music-graphics">
+      {!isAnimationStopped && (
+        <TitleButton
+          className="animated-title"
+          onMouseEnter={onMouseEnterHandler}
+          onMouseLeave={onMouseLeaveHandler}
+          onClick={onClickHandler}
+        >
+          {isMouseOnButton ? "Enter Here" : "Welcome!"}
+        </TitleButton>
+      )}
       {columnProps.map((columnProp, index) => (
         <MusicColumn key={index} {...columnProp} />
       ))}
@@ -45,6 +97,28 @@ const FlexWrapper = styled.div`
     &:last-of-type {
       margin-right: 0px;
     }
+  }
+`;
+
+const TitleButton = styled.button`
+  font-weight: bold;
+  background-color: transparent;
+  cursor: pointer;
+  outline: none;
+  appearance: none;
+  color: white;
+  font-size: 80px;
+  position: absolute;
+  border: 10px solid white;
+  border-radius: 5px;
+  padding: 10px 50px;
+  z-index: 9999;
+  top: 50%;
+
+  // TODO: Make the text color transparent
+  &:hover {
+    background-color: white;
+    color: black;
   }
 `;
 
