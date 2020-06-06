45d8e6b808e49db4b97009ffe3f024718f564e2c
hayate4th <diabolo.arte3000@gmail.com>
2020-06-07 00:15:16 +0900

    Add logo image and main title text

--- src/components/MainPortfolio/index.tsx
+++ src/components/MainPortfolio/index.tsx
@@ -1,7 +1,32 @@
 import React from "react";
+import styled from "styled-components";
+
+import logoImage from "../../assets/images/logoImage.jpg";
 
 const MainPortfolio: React.FC = () => {
-  return <div>Portfolio</div>;
+  return (
+    <Header>
+      <LogoImage src={logoImage} />
+      <MainTitle>hayate4th</MainTitle>
+    </Header>
+  );
 };
+
+const Header = styled.header`
+  display: flex;
+`;
+
+const LogoImage = styled.img`
+  border-radius: 50%;
+  width: 75px;
+  height: 75px;
+  margin-right: 10px;
+`;
+
+const MainTitle = styled.h1`
+  margin: 0;
+  vertical-align: middle;
+  align-self: center;
+`;
 
 export default MainPortfolio;
