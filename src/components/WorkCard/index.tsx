93a6449e098a9399a494b1902c387742cce5dcb8
hayate4th <diabolo.arte3000@gmail.com>
2020-06-22 23:53:47 +0900

    Add work title and description to WorkCard

--- src/components/WorkCard/index.tsx
+++ src/components/WorkCard/index.tsx
@@ -7,17 +7,39 @@ const WorkCard: React.FC = () => {
   return (
     <Wrapper>
       <Thumbnail src={BalloonSenderImg} />
+      <Contents>
+        <WorkTitle>balloon-sender v2</WorkTitle>
+        <WorkDescription>
+          Write whatever you want, tie it up to a balloon, and send it high into
+          the sky!
+        </WorkDescription>
+      </Contents>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
   border: 3px solid #000;
-  padding: 20px;
+  width: 250px;
 `;
 
 const Thumbnail = styled.img`
-  width: 200px;
+  display: block;
+  width: 250px;
+`;
+
+const Contents = styled.div`
+  padding: 10px;
+`;
+
+const WorkTitle = styled.h3`
+  font-size: 1.2rem;
+  margin-top: 0;
+  text-align: center;
+`;
+
+const WorkDescription = styled.p`
+  margin: 0;
 `;
 
 export default WorkCard;
